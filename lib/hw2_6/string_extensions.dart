cac34ed9aaaf043a3904aee93012cb21aca8c022
Dmitrii Sapronov <Dmitry.Sapronov@yandex.ru>
2022-01-24 00:12:55 +0300

    Решена 5я задача домашнего задания 2.6

--- lib/hw2_6/string_extensions.dart
+++ lib/hw2_6/string_extensions.dart
@@ -1,9 +1,37 @@
 extension StringExtensions on String {
+  static var stringToNumMap = <String, int>{
+    "zero":   0,
+    "one":    1,
+    "two":    2,
+    "three":  3,
+    "four":   4,
+    "five":   5,
+    "six":    6,
+    "seven":  7,
+    "eight":  8,
+    "nine":   9
+  };
+
   List<num> getNums() {
     return split(" ")
         .map((e) => e.trim())
         .map((e) => num.tryParse(e))
         .whereType<num>()
         .toList();
+  }
+
+  List<int> getIntList({required String delimiter}) {
+    var maybeNumbers = split(delimiter);
+    var result = <int>[];
+    for (String maybeNumber in maybeNumbers) {
+      if (stringToNumMap.containsKey(maybeNumber)) {
+        int number = stringToNumMap[maybeNumber]!;
+        if (result.contains(number)) {
+          continue;
+        }
+        result.add(number);
+      }
+    }
+    return result;
   }
 }
--- test/string_extensions_test.dart
+++ test/string_extensions_test.dart
@@ -2,7 +2,7 @@ import 'package:dart_basics/hw2_6/string_extensions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('StringExtensions', () {
+  group('getNums', () {
     test('nums in "1"', () {
       expect("1".getNums(), [1]);
     });
@@ -14,6 +14,18 @@ void main() {
     });
     test('nums in "-1 2.21 0"', () {
       expect("-1 2.21 0".getNums(), [-1, 2.21, 0]);
+    });
+  });
+
+  group('getIntList', () {
+    test('ints in "one, two, three, cat, dog"', () {
+      expect("one, two, three, cat, dog".getIntList(delimiter: ", "), [1, 2, 3]);
+    });
+    test('ints in "one, two, zero, zero"', () {
+      expect("one, two, zero, zero".getIntList(delimiter: ", "), [1, 2, 0]);
+    });
+    test('ints in "one, two, zero, zero, hi"', () {
+      expect("one, two, zero, zero, hi, nine".getIntList(delimiter: ", "), [1, 2, 0, 9]);
     });
   });
 }
